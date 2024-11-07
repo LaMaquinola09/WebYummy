ba21f475c62e51b6887cadd623a49211e5b3d302
CieloMorales <115675392+CieloMorales@users.noreply.github.com>
2024-11-07 16:21:43 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -51,8 +51,6 @@ Route::group(['middleware' => ['auth', 'check.restaurant.active']], function () 
         return view('dashboard');
     })->middleware(['auth', 'verified'])->name('dashboard');
 
-    // Te amo Cielo
-    // Atte Fernando
     //Rutas para el pedidos
     Route::get('/pedidos', [PedidoController::class, 'index'])->name('pedidos.index');
     // Ruta para obtener los pedidos
